21e14ae09f6811a50b63136a504ee581baa6d4d5
김명철 <mclkim@blueweb.co.kr>
2015-12-30 15:04:38 +0900

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   .buildpath
    	modified:   .gitignore
    	deleted:    kaiser/Plupload
    	new file:   kaiser/Plupload/PluploadHandler.php
    	new file:   kaiser/Plupload/README.md
    	new file:   kaiser/Plupload/upload.php
    	new file:   test/bootstrap.php

---
+++ kaiser/Plupload/upload.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../src/Kaiser/Plupload/PluploadHandler.php");
+
+PluploadHandler::no_cache_headers();
+PluploadHandler::cors_headers();
+
+if (!PluploadHandler::handle(array(
+	'target_dir' => 'uploads/',
+	'allow_extensions' => 'jpg,jpeg,png'
+))) {
+	die(json_encode(array(
+		'OK' => 0, 
+		'error' => array(
+			'code' => PluploadHandler::get_error_code(),
+			'message' => PluploadHandler::get_error_message()
+		)
+	)));
+} else {
+	die(json_encode(array('OK' => 1)));
+}

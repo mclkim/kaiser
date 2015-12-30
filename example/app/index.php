747b1e100f5e8e1ca254fad9c38ef9bb33abb1e3
김명철 <mclkim@blueweb.co.kr>
2015-12-30 18:07:47 +0900

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   example/app/hello.php
    	modified:   example/app/index.php
    	modified:   example/index.php

--- example/app/index.php
+++ example/app/index.php
@@ -1,10 +1,13 @@
 <?php
 use \Kaiser\Controller;
+/**
+ * http://localhost/test/public/?index
+ */
 class index extends Controller {
 	protected function requireLogin() {
 		return false;
 	}
 	function execute() {
-		echo 'hello world';
+		echo 'Kaiser PHP framework~~';
 	}
 }
--- example/index.php
+++ example/index.php
@@ -2,24 +2,16 @@
 
 /**
  * Step 1: Require the Kaiser Framework using Composer's autoloader
- *
- * If you are not using Composer, you need to load Kaiser Framework with your own
- * PSR-4 autoloader.
  */
 require __DIR__ . '/../vendor/autoload.php';
 
 /**
  * Step 2: Instantiate a Kaiser application
- *
- * This example instantiates a Kaiser application using
- * its default settings. However, you will usually configure
- * your Kaiser application now by passing an associative array
- * of setting names and values into the application constructor.
  */
 $app = new Kaiser\App ();
 
 /**
- * Step 3:
+ * Step 3: Setting Kaiser application Controller
  */
 $app->setAppDir ( [ 
 		__DIR__ . '/app' 
@@ -27,8 +19,5 @@ $app->setAppDir ( [
 
 /**
  * Step 4: Run the Kaiser application
- *
- * This method should be called last. This executes the Kaiser application
- * and returns the HTTP response to the HTTP client.
  */
 $app->run ();

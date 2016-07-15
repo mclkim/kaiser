b68cdee81274379bca42cfdc4deb0e91d7c9a9df
Kim myungchul <mclkims@gmail.com>
2016-07-15 22:12:05 +0900

    modified:   src/Manager/DBManager.php 	modified:   src/Manager/Pagination.php

--- src/Manager/DBManager.php
+++ src/Manager/DBManager.php
@@ -123,29 +123,91 @@ class DBManager extends \Pixie\QueryBuilder\QueryBuilderHandler
     /**
      * return $this->table($table)->insert($data);
      */
-    function AutoExecuteInsert($table, $data)
-    {
-        $instance = $this->table($table);
-        $queryObject = $instance->getQuery('insert', $data);
-        $sql = $queryObject->getSql();
-        $params = $queryObject->getBindings();
-        list($result) = $this->statement($sql, $params);
-        $this->debug($this->executeEmulateQuery($sql, $params));
-        $res = $this->pdo->lastInsertId();
-        return $res === '0' ? $result->rowCount() : $res;
+    // function AutoExecuteInsert($table, $data)
+    // {
+    //     $instance = $this->table($table);
+    //     $queryObject = $instance->getQuery('insert', $data);
+    //     $sql = $queryObject->getSql();
+    //     $params = $queryObject->getBindings();
+    //     list($result) = $this->statement($sql, $params);
+    //     $this->debug($this->executeEmulateQuery($sql, $params));
+    //     $res = $this->pdo->lastInsertId();
+    //     return $res === '0' ? $result->rowCount() : $res;
 
+    // }
+
+    // function AutoExecuteUpdate($table, $data, $key1, $oper1 = null, $val1 = null, $key2 = null, $oper2 = null, $val2 = null)
+    // {
+    //     if (func_num_args() == 4) {
+    //         return $this->table($table)->where($key1, '=', $oper1)->update($data);
+    //     } else if (func_num_args() == 5) {
+    //         return $this->table($table)->where($key1, $oper1, $val1)->update($data);
+    //     } else if (func_num_args() == 6) {
+    //         return $this->table($table)->where($key1, '=', $oper1)->where($val1, '=', $key2)->update($data);
+    //     } else if (func_num_args() == 8) {
+    //         return $this->table($table)->where($key1, $oper1, $val1)->where($key2, $oper2, $val2)->update($data);
+    //     }
+    // }
+    function AutoExecuteInsert($table_name, $fields_values, $where = false)
+    {
+        $ret = $this->_buildManipSQL($table_name, $fields_values, DB_AUTO_INSERT, $where);
+        return $this->executePreparedUpdate($ret ['query'], $ret ['params']);
     }
 
-    function AutoExecuteUpdate($table, $data, $key1, $oper1 = null, $val1 = null, $key2 = null, $oper2 = null, $val2 = null)
+    function AutoExecuteUpdate($table_name, $fields_values, $where = false)
     {
-        if (func_num_args() == 4) {
-            return $this->table($table)->where($key1, '=', $oper1)->update($data);
-        } else if (func_num_args() == 5) {
-            return $this->table($table)->where($key1, $oper1, $val1)->update($data);
-        } else if (func_num_args() == 6) {
-            return $this->table($table)->where($key1, '=', $oper1)->where($val1, '=', $key2)->update($data);
-        } else if (func_num_args() == 8) {
-            return $this->table($table)->where($key1, $oper1, $val1)->where($key2, $oper2, $val2)->update($data);
+        $ret = $this->_buildManipSQL($table_name, $fields_values, DB_AUTO_UPDATE, $where);
+        return $this->executePreparedUpdate($ret ['query'], $ret ['params']);
+    }
+
+    private function _buildManipSQL($table, $table_fields, $mode, $where = false)
+    {
+        if (count($table_fields) == 0) {
+            return false;
+        }
+        $first = true;
+        $params = array();
+        switch ($mode) {
+            case DB_AUTO_INSERT :
+                $values = '';
+                $fields = '';
+                foreach ($table_fields as $field => $value) {
+                    $params [] = $value;
+                    if ($first) {
+                        $first = false;
+                    } else {
+                        $fields .= ',';
+                        $values .= ',';
+                    }
+                    $fields .= $field;
+                    $values .= '?';
+                }
+                $sql = "INSERT INTO $table ($fields) VALUES ($values)";
+                return array(
+                    'query' => $sql,
+                    'params' => $params
+                );
+            case DB_AUTO_UPDATE :
+                $set = '';
+                foreach ($table_fields as $field => $value) {
+                    $params [] = $value;
+                    if ($first) {
+                        $first = false;
+                    } else {
+                        $set .= ',';
+                    }
+                    $set .= "$field = ?";
+                }
+                $sql = "UPDATE $table SET $set";
+                if ($where) {
+                    $sql .= " WHERE $where";
+                }
+                return array(
+                    'query' => $sql,
+                    'params' => $params
+                );
+            default :
+                return false;
         }
     }
 }

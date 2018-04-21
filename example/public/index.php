52da51bd20bf9d3f442e1a5c4d91285d85f5a19e
mclkim <mclkim@naver.com>
2018-04-22 07:22:04 +0900

    new file:   example/public/index.php

---
+++ example/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Step 1: Require the Kaiser Framework using Composer's autoloader
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Step 2: Instantiate a Kaiser application
+ */
+$app = new Kaiser\App ();
+
+/**
+ * Step 3: Setting Kaiser application Controller
+ */
+$app->setAppDir([
+    __DIR__ . '/../app'
+]);
+
+/**
+ * Step 4: Run the Kaiser application
+ */
+$app->run();

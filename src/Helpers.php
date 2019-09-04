3d279313f93dae531ebca172d37f6cda40b8f6d4
김명철 <mclkim@naver.com>
2019-09-04 12:49:51 +0900

    modified:   src/Helpers.php

--- src/Helpers.php
+++ src/Helpers.php
@@ -174,3 +174,37 @@ if (!function_exists('ends_with')) {
         return strrpos($haystack, $needle) === strlen($haystack) - strlen($needle);
     }
 }
+/**
+ * ---------------------------------------------------------------
+ * 정규분포
+ * ---------------------------------------------------------------
+ */
+// https://stackoverflow.com/questions/7224437/php-statistics-z-score-normal-distributions
+// http://web.stanford.edu/class/cs109/demos/cdf.html
+if (!function_exists('cumnormdist')) {
+    function cumnormdist($x)
+    {
+        $b1 = 0.319381530;
+        $b2 = -0.356563782;
+        $b3 = 1.781477937;
+        $b4 = -1.821255978;
+        $b5 = 1.330274429;
+        $p = 0.2316419;
+        $c = 0.39894228;
+
+        if ($x >= 0.0) {
+            $t = 1.0 / (1.0 + $p * $x);
+            return (1.0 - $c * exp(-$x * $x / 2.0) * $t * ($t * ($t * ($t * ($t * $b5 + $b4) + $b3) + $b2) + $b1));
+        } else {
+            $t = 1.0 / (1.0 - $p * $x);
+            return ($c * exp(-$x * $x / 2.0) * $t * ($t * ($t * ($t * ($t * $b5 + $b4) + $b3) + $b2) + $b1));
+        }
+    }
+}
+if (!function_exists('z')) {
+    /* Z Scores */
+    function z($var, $mean, $std)
+    {
+        return ($var - $mean) / $std;
+    }
+}

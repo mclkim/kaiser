7e5c242e99808d8cc6c17b81f99f7ace31e89a35
김명철 <mclkim@naver.com>
2019-12-14 05:13:10 +0900

    modified:   example/public/index.php

--- example/public/index.php
+++ example/public/index.php
@@ -1,16 +1,42 @@
 <?php
 
-require_once '../vendor/autoload.php';
+use DI\ContainerBuilder;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Factory\AppFactory;
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Mcl\Kaiser\App(['settings' => ['displayErrorDetails' => true]]);
+$containerBuilder = new ContainerBuilder();
 
-//// Define app routes
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello " . $args['name']);
-    return $response;
-});
+// configure PHP-DI here
+$containerBuilder->addDefinitions([
+    'settings' => [
+        'displayErrorDetails' => true, // Should be set to false in production
+    ],
+]);
 
-$app->run('../app');
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$container = $app->getContainer();
+
+//TODO::KAR-START
+$request = ServerRequestFactory::createFromGlobals();
+$path = $request->getUri()->getPath();
+
+$router = new \Mcl\Kaiser\Router($path);
+$router->setAppMap(__DIR__ . '../app');
+$routeInfo = $router->dispatch();
+
+if (is_array($routeInfo) && $routeInfo[0] == \Mcl\Kaiser\Router::FOUND) {
+    $callable = new $routeInfo[1] ($container);
+    if ($callable instanceof \Mcl\Kaiser\ControllerInterface) {
+        $app->map($callable->methods(), $path, [$callable, $routeInfo[2]])->add(new \Mcl\Kaiser\Auth($callable));
+    }
+}
+//TODO::KAR-END
+
+$app->run();

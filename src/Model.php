0554ab84db2918e4f4b53411cae40b46fe69d0ae
mclkim <mc.kim@ionebio.com>
2021-05-28 14:50:43 +0900

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   composer.json
    	modified:   src/Model.php

--- src/Model.php
+++ src/Model.php
@@ -15,6 +15,8 @@ use Psr\Log\LoggerInterface;
 class Model
 {
     protected $container;
+    protected $original_memory_limit;
+    protected $original_max_execution_time;
 
     public function __construct(ContainerInterface $container = null)
     {
@@ -65,5 +67,25 @@ class Model
     {
         $logger = $this->container->get(LoggerInterface::class);
         $logger->error($message, $context);
+    }
+
+    protected function begin()
+    {
+        // on the beginning of your script save original memory limit
+        $this->original_memory_limit = ini_get('memory_limit');
+
+        // then set it to the value you think you need (experiment)
+        ini_set('memory_limit', '256M');
+
+        $this->original_max_execution_time = ini_get('max_execution_time');
+        ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+    }
+
+    protected function end()
+    {
+        // at the end of the script set it to it's original value
+        // (if you forget this PHP will do it for you when performing garbage collection)
+        ini_set('memory_limit', $this->original_memory_limit);
+        ini_set('max_execution_time', $this->original_max_execution_time);
     }
 }
